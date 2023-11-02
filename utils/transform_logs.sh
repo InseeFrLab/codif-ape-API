3094f9cea57abd5ed6900892f94103b1d63affcf
ThomasFaria <thomas.faria@ensae.fr>
2023-11-02 15:00:19 +0000

    remove unecessary command

--- utils/transform_logs.sh
+++ utils/transform_logs.sh
@@ -4,7 +4,6 @@ PATH_TO_LOGS=/api
 LOCAL_PATH_LOGS=codification_ape_log_file.log
 START_DATE=$(date -d "yesterday 00:0:00" '+%Y-%m-%d %H:%M:%S')
 END_DATE=$(date -d "yesterday 23:59:59" '+%Y-%m-%d %H:%M:%S')
-export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 
 # Copy locally log file from API pod
 kubectl cp $NAMESPACE/$POD_NAME:$PATH_TO_LOGS/codification_ape_log_file.log $LOCAL_PATH_LOGS

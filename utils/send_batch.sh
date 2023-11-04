8248972e939a00ec9d52acc99b39659c2a8ef405
ThomasFaria <thomas.faria@ensae.fr>
2023-11-04 10:11:39 +0000

    fix another bug

--- utils/send_batch.sh
+++ utils/send_batch.sh
@@ -3,4 +3,4 @@ LOG_FILE_PATH=$NAMESPACE/log_files/preprocessed/
 DATE_TO_LOG=$(date --date="-49days" +%Y-%m-%d)
 
 # Send a batch query from a daily log
-python send_batch.py $LOG_FILE_PATH $CURRENT_DATE
+python send_batch.py $LOG_FILE_PATH $DATE_TO_LOG

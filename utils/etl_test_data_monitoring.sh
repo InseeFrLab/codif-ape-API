02fee03312d8732b0bca5887bbfba5a98705f9f6
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-03-01 14:28:52 +0000

    refer path to branch for testing in raw files'

--- utils/etl_test_data_monitoring.sh
+++ utils/etl_test_data_monitoring.sh
@@ -8,7 +8,7 @@ mc ls s3/$S3_BUCKET/$PATH_ANNOTATION_RESULTS | awk '{print "s3/'$S3_BUCKET'/'$PA
 python extract_test_data.py $DATA_FILE_PATH_LOCAL $PATH_ANNOTATION_PREPROCESSED
 
 # Predict with current model to send data to dashboard
-python send_batch_dashboard.py $S3_BUCKET/$PATH_ANNOTATION_PREPROCESSED $PATH_ANNOTATION_DASHBOARD/current-model $CURRENT_MODEL_API_PATH
+python send_batch_test_data.py $S3_BUCKET/$PATH_ANNOTATION_PREPROCESSED $PATH_ANNOTATION_DASHBOARD/current-model $CURRENT_MODEL_API_PATH
 
 # Predict with next/retrained model to send data to dashboard
-python send_batch_dashboard.py $S3_BUCKET/$PATH_ANNOTATION_PREPROCESSED $PATH_ANNOTATION_DASHBOARD/next-model $NEXT_MODEL_API_PATH
+python send_batch_test_data.py $S3_BUCKET/$PATH_ANNOTATION_PREPROCESSED $PATH_ANNOTATION_DASHBOARD/next-model $NEXT_MODEL_API_PATH

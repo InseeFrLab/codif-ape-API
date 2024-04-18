aa00aea723a13e1a6a7832ecf533af8d8874b1ca
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-04-18 23:20:10 +0000

    add quote schedule

--- utils/extract_db.sh
+++ utils/extract_db.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 # sampling from extracted db
 EXTRACT_DB=$(python extract_db.py "$NAMESPACE/extractions" $NUMBER_TO_ANNOTATE $MONTH_INTERVAL)
-mc mv "$EXTRACT_DB" "$DATA_SAMPLED_QUEUE_PATH"
-mc cp --recursive "$DATA_SAMPLED_QUEUE_PATH" "$DATA_SAMPLED_ARCHIVE_PATH"
+mc mv $EXTRACT_DB $DATA_SAMPLED_QUEUE_PATH
+mc cp --recursive $DATA_SAMPLED_QUEUE_PATH $DATA_SAMPLED_ARCHIVE_PATH

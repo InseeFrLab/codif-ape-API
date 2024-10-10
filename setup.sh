db7ec7262b0a6a2f9825fd257b181f57fdd6b40b
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2024-10-10 14:42:52 +0000

    modified:   .github/workflows/build-image.yaml 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ pre-commit install
 export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
 export MLFLOW_TRACKING_URI=https://user-meilametayebjee-mlflow.user.lab.sspcloud.fr
 export MLFLOW_MODEL_NAME=fasttext-pytorch
-export MLFLOW_MODEL_VERSION=2
+export MLFLOW_MODEL_VERSION=7
 export API_USERNAME=username
 export API_PASSWORD=password
 export AUTH_API=False

b7e60ca196860c35f6697e9eae7c531e235afea6
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-05-19 16:04:24 +0000

    fix(wrapper)!:  test + handle params and outputs
    clean and test the API

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ unset AWS_SESSION_TOKEN
 
 export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr
-export MLFLOW_MODEL_NAME=FastText-pytorch
-export MLFLOW_MODEL_VERSION="2"
+export MLFLOW_MODEL_NAME=test_wrapper_pytorch
+export MLFLOW_MODEL_VERSION="10"
 export API_USERNAME=username
 export API_PASSWORD=password
 export AUTH_API=False
--- src/api/routes/predict.py
+++ src/api/routes/predict.py
@@ -1,32 +1,28 @@
 from typing import Annotated, List
 
-import pandas as pd
 from fastapi import APIRouter, Depends, Request
 from fastapi.security import HTTPBasicCredentials
 
-from api.models.forms import SingleForm
+from api.models.forms import BatchForms
 from api.models.responses import PredictionResponse
-from utils.preprocessing import mappings, preprocess_inputs
+from utils.preprocessing import mappings
 from utils.security import get_credentials
-
-router = APIRouter(prefix="/single", tags=["Predict an activity"])
 
 APE_NIV5_MAPPING = mappings["nace2025"]
 INV_APE_NIV5_MAPPING = {v: k for k, v in APE_NIV5_MAPPING.items()}
 
-router = APIRouter(prefix="/batch", tags=["Predict a batch of activity"])
+router = APIRouter(prefix="/predict", tags=["Predict NACE code for a list of activities"])
 
 
 @router.post("/predict", response_model=List[PredictionResponse])
 async def predict(
     credentials: Annotated[HTTPBasicCredentials, Depends(get_credentials)],
     request: Request,
-    forms: list[SingleForm],
-    params_dict: dict = {
-        "nb_echos_max": 5,
-        "prob_min": 0.01,
-        "dataloader_params": {"num_workers": 0, "pin_memory": False, "persistent_workers": False},
-    },
+    forms: BatchForms,
+    nb_echos_max: int = 5,
+    prob_min: float = 0.01,
+    num_workers: int = 1,
+    batch_size: int = 1,
 ):
     """
     Endpoint for predicting batches of data.
@@ -36,12 +32,27 @@ async def predict(
         forms (Forms): The input data in the form of Forms object.
         nb_echos_max (int, optional): The maximum number of predictions to return. Defaults to 5.
         prob_min (float, optional): The minimum probability threshold for predictions. Defaults to 0.01.
+        num_workers (int, optional): Number of CPU for multiprocessing in Dataloader. Defaults to 1.
+        batch_size (int, optional): Size of a batch for batch prediction.
+
+    For single predictions, we recommend keeping num_workers and batch_size to 1 for better performance.
+    For batched predictions, consider increasing these two parameters (num_workers can range from 4 to 12, batch size can be increased up to 256) to optimize performance.
 
     Returns:
         list: The list of predicted responses.
     """
-    query = preprocess_inputs(forms.forms)
+    input_data = forms.forms
 
-    input_data = pd.DataFrame(query)
+    params_dict = {
+        "nb_echos_max": nb_echos_max,
+        "prob_min": prob_min,
+        "dataloader_params": {
+            "pin_memory": False,
+            "persistent_workers": False,
+            "num_workers": num_workers,
+            "batch_size": batch_size,
+        },
+    }
 
-    return request.app.state.model.predict(input_data, params=params_dict)
+    output = request.app.state.model.predict(input_data, params=params_dict)
+    return [out.model_dump() for out in output]

d5e5bb13a72f205dc01d04d91d9b28f9f2fa8cb7
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-06-17 16:42:59 +0000

    fix: bump model version

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ uv run -m nltk.downloader stopwords
 export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr
 export MLFLOW_MODEL_NAME=FastText-pytorch-2025
-export MLFLOW_MODEL_VERSION="4"
+export MLFLOW_MODEL_VERSION="6"
 export API_USERNAME=username
 export API_PASSWORD=password
 export AUTH_API=False

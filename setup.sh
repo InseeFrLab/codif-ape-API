34a1fd67cdd1acd3a9aa6c764dd5dc3035a8f8c7
TheAIWizard <nathanpanoel@hotmail.fr>
2026-06-08 17:38:08 +0000

    feat: init adapt breaking changes > need check S3 conn mlstore-bucket

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ export MLFLOW_S3_ENDPOINT_URL="https://$AWS_S3_ENDPOINT"
 export MLFLOW_TRACKING_URI=https://projet-ape-mlflow.user.lab.sspcloud.fr
 export MLFLOW_MODEL_NAME=FastText-pytorch-2025
 export MLFLOW_MODEL_VERSION="6"
+# export MLFLOW_ALLOW_FILE_STORE=true
 export API_USERNAME=username
 export API_PASSWORD=password
 export AUTH_API=False
